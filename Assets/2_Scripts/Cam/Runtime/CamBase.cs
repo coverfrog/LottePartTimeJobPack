5bfebab2a507f93442e3d1569bb462333fa8cf40
coverfrog <justfroglab@gmail.com>
2025-07-04 21:24:22 +0900

    너무 안티 패턴

--- Assets/2_Scripts/Cam/Runtime/CamBase.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public abstract class CamBase : ScriptableObject
-{
-    public abstract void OnLateUpdate(Cam cam);
-}
--- Assets/2_Scripts/Global/Runtime/GlobalHandler.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public abstract class GlobalHandler : MonoBehaviour
-{
-    public abstract void Setup(GlobalManager globalManager);
-}
